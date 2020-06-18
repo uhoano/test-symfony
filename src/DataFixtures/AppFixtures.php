bbda0475e00c94a827c19740e6af943b3aa5ee43
Jean-Charles <jean-charles.ly@oika.eu>
2020-06-18 12:01:04 +0200

    Add User table

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,36 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    private function encode($user, $plaintextpassword)
+    {
+        return $this->passwordEncoder->encodePassword(
+            $user,
+            $plaintextpassword
+        );
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $faker = Factory::create();
+        for ($i = 0; $i < 10; $i++) {
+            $simpleUser = new User();
+            $simpleUser->setNickname(strtolower($faker->firstName()));
+            $simpleUser->setPassword($this->encode($simpleUser, "mdp"));
+            $simpleUser->setRoles(['ROLE_USER']);
+            $manager->persist($simpleUser);
+        }
         $manager->flush();
     }
 }
